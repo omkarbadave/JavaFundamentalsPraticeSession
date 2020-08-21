44759544c09f668f8f13944b610d945da19ce70e
omkarb2 <omkarb2@kpit.com>
2020-08-21 22:46:50 +0530

    #Task5 : Added main class to run the phone book application.

--- PhoneAddressBook/src/com/omkar/phonebook/RunApplication.java
+++ PhoneAddressBook/src/com/omkar/phonebook/RunApplication.java
@@ -1,10 +1,13 @@
 package com.omkar.phonebook;
 
+import com.omkar.phonebook.controllers.PhoneBookController;
+
 public class RunApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PhoneBookController pc = new PhoneBookController();
+		pc.run();
 	}
 
 }
