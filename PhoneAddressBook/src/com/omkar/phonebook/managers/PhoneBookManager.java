a248ddbf99d0cf827bd38e7c45274e08b327ef44
omkarb2 <omkarb2@kpit.com>
2020-08-21 22:44:35 +0530

    #Task3 :  Added a manager for which implements crud interface.

---
+++ PhoneAddressBook/src/com/omkar/phonebook/managers/PhoneBookManager.java
@@ -0,0 +1,71 @@
+package com.omkar.phonebook.managers;
+
+import java.util.List;
+
+import com.omkar.phonebook.interfaces.PhoneBookCRUD;
+import com.omkar.phonebook.pojos.Contact;
+import com.omkar.phonebook.pojos.PhoneBook;
+
+public class PhoneBookManager implements PhoneBookCRUD<Contact> {
+	
+	PhoneBook pb;		
+	
+	public PhoneBookManager(PhoneBook pb) {
+		super();
+		this.pb = pb;
+	}
+
+	@Override
+	public List<Contact> getAll() {
+		// TODO Auto-generated method stub
+		return this.pb.getContactsList();
+		//return null;
+	}
+
+	@Override
+	public Contact getDetails(String name) {
+		// TODO Auto-generated method stub
+		 for(Contact c:this.pb.getContactsList()) {
+			 if(c.getFirst_name().contentEquals(name)) {
+				 return c;
+			 }
+		 }		
+		return null;
+	}
+
+	@Override
+	public void add(Contact data) {
+		// TODO Auto-generated method stub
+		this.pb.getContactsList().add(data);		
+	}
+
+	@Override
+	public Contact delete(int id) {
+		// TODO Auto-generated method stub
+		Contact c = this.find(id);
+		if(this.pb.getContactsList().remove(c)) {
+			return c;		
+		}
+		return null;
+	}
+	
+	public Contact find(String name) {
+		for(Contact c:this.pb.getContactsList()) {
+			 if(c.getFirst_name().contentEquals(name)) {
+				 return c;
+			 }
+		 }
+		return null;
+	}
+	
+	public Contact find(int id) {
+		for(Contact c:this.pb.getContactsList()) {
+			 if(c.getId() == id) {
+				 return c;
+			 }
+		 }
+		return null;
+	}
+	
+
+}
